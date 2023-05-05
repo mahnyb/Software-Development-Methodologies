228582149bb92abc6bab3f2cc98f0fa830affdbf
Mahny <87762240+mahnyb@users.noreply.github.com>
2023-05-05 16:36:58 +0300

    Create Spa.java

---
+++ Assignment 7/Spa.java
@@ -0,0 +1,32 @@
+
+// Mahny Barazandehtar - 20210702004 - Section 1 - CSE 212 - Assignment 7
+
+public class Spa extends Service {
+
+	int days;
+	double spaCost;
+	
+	public Spa(int customerID, int days, double spaCost ) {
+		this.customerID = customerID;
+		this.days = days;
+		this.spaCost = spaCost;
+	}
+	
+	@Override
+	public String ServiceType() {
+		
+		return "Spa";
+	}
+
+	@Override
+	public double calculateService() {
+
+		return 100 * days;
+	}
+
+	@Override
+	public double getCost() {
+		return calculateService();
+	}
+
+}
