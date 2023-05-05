9b72f31f0db179302e765e507e53f33a554125d1
Mahny <87762240+mahnyb@users.noreply.github.com>
2023-05-05 16:46:19 +0300

    Create Service.java

---
+++ Assignment 8/Service.java
@@ -0,0 +1,23 @@
+
+// Mahny Barazandehtar - 20210702004 - Section 1 - CSE 212 - Assignment 8
+
+public abstract class Service implements Calculable {
+	
+	public int customerID;
+	public abstract String getServiceType();
+	public abstract double calculateService(); 
+	
+	public int getCustomerID(){  
+		return customerID;
+	}
+	public void setCustomerID(int customerID) {  
+		this.customerID = customerID;
+	}
+	
+	public void displayServiceInfo(){
+		
+		System.out.printf("Customer ID: %d, Service Type: %s, Cost: %.2f\n", getCustomerID(), getServiceType(), calculateService());
+	}
+
+
+}
