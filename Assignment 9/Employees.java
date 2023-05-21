f2e74fa19c2caec38d5bb478deb6809ff3cfcee2
Mahny <87762240+mahnyb@users.noreply.github.com>
2023-05-21 15:49:58 +0300

    Create Employees.java

---
+++ Assignment 9/Employees.java
@@ -0,0 +1,64 @@
+
+// Mahny Barazandehtar - 20210702004 - Section 1 - CSE 212 - Assignment 9
+
+public class Employees implements Calculable{
+	
+	public String Name;
+	public String Surname;
+	public double MonthlyPayment;
+	public int ID;
+	
+	public Employees(String Name, String Surname, double MonthlyPayment, int ID) {
+		
+		this.Name = Name;
+		this.Surname = Surname;
+		this.MonthlyPayment = MonthlyPayment;
+		this.ID = ID;
+	}
+
+	// name
+	public String getName() {
+		return Name;
+	}
+	public void setName(String Name) {
+		
+		this.Name = Name;
+	}
+	
+	// surname
+	public String getSurname() {
+		return Surname;
+	}
+	public void setSurname(String Surname) {
+		
+		this.Surname = Surname;
+	}
+	
+	// monthly payment
+	public double getMonthlyPayment() {
+		return MonthlyPayment;
+	}
+	public void setMonthlyPayment(double MonthlyPayment) {
+		
+		this.MonthlyPayment = MonthlyPayment;
+	}
+	
+	// ID
+	public int getID() {
+		return ID;
+	}
+	public void setID(int ID) {
+		
+		this.ID = ID;
+	}
+	
+	
+	@Override
+	public double getCost() {
+		
+		return MonthlyPayment;
+	}
+	
+	
+
+}
