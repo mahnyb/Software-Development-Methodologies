71d0841ff63e285b1d1e9b64bf67ddc2e35b05fd
Mahny <87762240+mahnyb@users.noreply.github.com>
2023-05-21 15:56:34 +0300

    Create Reservation.java

---
+++ Assignment 10/Reservation.java
@@ -0,0 +1,142 @@
+
+// Mahny Barazandehtar - 20210702004 - Section 1 - CSE 212 - Assignment 10
+
+import java.util.*;  
+
+public class Reservation extends Service implements Comparable<Reservation> {
+	static int totalNumOfReservations = 0;
+	private Room room;
+	private String hotelName;
+	private String cityName;
+	private String reservationMonth;
+	private int reservationStart;
+	private int reservationEnd;
+	//int dailyCost;
+	
+	public Reservation(String cityName, String hotelName, String reservationMonth, int reservationStart, int reservationEnd) {
+        
+		this.setCityName(cityName);
+		this.setHotelName(hotelName);    //this.hotelName = hotelName;
+        /*
+        // to create rooms
+        
+        	if (roomType.equals("Single")) {
+            	room = new Single();
+            }
+            else if (roomType.equals("Double")) {
+            	room = new Double();
+            }
+            else if (roomType.equals("Club")) {
+            	room = new Club();
+            }
+            else if (roomType.equals("Family")) {
+            	room = new Family();
+            }
+            else if (roomType.equals("Family with View")) {
+            	room = new FamilyView();
+            }
+            else if (roomType.equals("Suite")) {
+            	room = new Suite();
+            }
+            */
+        this.setReservationMonth(reservationMonth);  //this.reservationMonth = reservationMonth;
+        this.setReservationStart(reservationStart); //this.reservationStart = reservationStart;
+        this.setReservationEnd(reservationEnd);    //this.reservationEnd = reservationEnd;
+        //this.setDailyCost(reservationEnd);      //this.dailyCost = room.getCost();
+        totalNumOfReservations++;
+        
+        setCustomerID(totalNumOfReservations);
+	}
+	
+	// City Name
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	
+    
+	// Hotel Name
+    public String getHotelName() {
+        return hotelName;
+    }
+    public void setHotelName(String hotelName) {
+    	this.hotelName = hotelName;
+    }
+    
+    // Room
+    public Room getRoom() {
+    	return room;
+    }
+    public void setRoom(Room room) {
+    	this.room = room;
+    }
+    
+    // Reservation Month
+    public String getReservationMonth() {
+        return reservationMonth;
+    }
+    public void setReservationMonth(String reservationMonth) {
+    	this.reservationMonth = reservationMonth;
+    }
+    
+    // Reservation Start
+    public int getReservationStart() {
+        return reservationStart;
+    }
+    public void setReservationStart(int reservationStart) {
+    	this.reservationStart = reservationStart;
+    }
+    
+    // Reservation End
+    public int getReservationEnd() {
+        return reservationEnd;
+    }
+    public void setReservationEnd(int reservationEnd) {
+    	this.reservationEnd = reservationEnd;
+    }
+    /*
+    // Daily Cost
+    public int getDailyCost() {
+        return dailyCost;
+    }
+    public void setDailyCost(int dailyCost) {
+    	this.dailyCost = dailyCost;
+    }
+    */
+    // Total Price
+    public double calculateService() {
+        int duration = getReservationEnd() - getReservationStart();
+        int multiplier = 1;
+        if (getReservationMonth().toLowerCase().equals("june") || getReservationMonth().toLowerCase().equals("july") || getReservationMonth().toLowerCase().equals("august")) {
+            multiplier = 2;
+        }
+        return duration * getRoom().getCost() * multiplier;
+        
+    }
+    
+    public String displayInfo() {
+       return "Reservation ID #" + getCustomerID() + "\n" + "Reservation at " + getHotelName() + " starts on " + getReservationMonth() + " " + getReservationStart() + " and ends on " + getReservationMonth() + " " + getReservationEnd();
+    }
+
+    @Override
+	public String getServiceType() {
+		 
+		return "Room Booking"; 
+	}
+
+	@Override
+	public double getCost() {
+		
+		return calculateService();
+	}
+
+	@Override
+	public int compareTo(Reservation o) {
+		
+		return this.hotelName.compareTo(o.hotelName);
+	}
+	
+}
+	
