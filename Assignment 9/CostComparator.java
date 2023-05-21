d30e03ec645c0357fede3d39c2bef6fc946b84ce
Mahny <87762240+mahnyb@users.noreply.github.com>
2023-05-21 15:49:22 +0300

    Create CostComparator.java

---
+++ Assignment 9/CostComparator.java
@@ -0,0 +1,30 @@
+
+// Mahny Barazandehtar - 20210702004 - Section 1 - CSE 212 - Assignment 9
+
+import java.util.*;  
+
+public class CostComparator implements Comparator<Service>{
+
+	
+	@Override
+	public int compare(Service o1, Service o2) {  
+		
+		double costo1, costo2, comparisonResult;
+		
+		costo1 = o1.getCost();
+		costo2 = o2.getCost();
+		
+		comparisonResult = costo1 - costo2;
+		
+		if(comparisonResult > 0) {
+			return 1;
+		}
+		else if(comparisonResult < 0){
+			return -1;
+		}
+		else{
+			return 0;
+		}
+		
+	}
+}
