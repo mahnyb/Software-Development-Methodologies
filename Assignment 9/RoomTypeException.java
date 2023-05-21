d4b94f2da6faf2ee2aff59c62ad98a5b133cb630
Mahny <87762240+mahnyb@users.noreply.github.com>
2023-05-21 15:46:14 +0300

    Create RoomTypeException.java

---
+++ Assignment 9/RoomTypeException.java
@@ -0,0 +1,11 @@
+
+// Mahny Barazandehtar - 20210702004 - Section 1 - CSE 212 - Assignment 9
+
+public class RoomTypeException extends Exception {
+
+	public RoomTypeException(String errorMessage) {
+		
+		super(errorMessage);
+	}
+	
+}
